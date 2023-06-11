4a0bd58bb076ebd9c27dcfacc8d56b6ec42940ce
JamesCordova <jcordovas@unsa.edu.pe>
2023-06-11 09:24:15 -0500

    Clase Test para probar los metodos en la clase double link list

---
+++ Ejercicio2/Test.java
@@ -0,0 +1,6 @@
+public class Test{
+  public static void main(String[] args){
+    DoubleLinkList<Integer> nums = new DoubleLinkList<Integer>();
+    System.out.println("Esta vacio? " + nums.isEmpty());
+  }
+}

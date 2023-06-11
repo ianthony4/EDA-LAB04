563f44283b87e2dc5568908cb67721b9c4207db9
JamesCordova <jcordovas@unsa.edu.pe>
2023-06-11 12:00:11 -0500

    Metodo para usar el ordenamiento por insercion

--- Ejercicio2/Test.java
+++ Ejercicio2/Test.java
@@ -11,4 +11,22 @@ public class Test{
     }
     return nums;
   }
+  public static long insertionSort(int[] A) {
+    int key;
+    int i;
+    long nano_startTime = System.nanoTime();
+    for(int j=1; j<A.length; j=j+1) {
+      key = A[j];
+      //Insertar A[j] en la secuencia ordenada A[1..j-1]
+      i = j-1;
+      while(i>-1 && A[i]>key) {
+        A[i+1] = A[i];
+        i = i-1;
+      }
+      A[i+1] = key;
+      //imprimirArreglo(A);
+    }
+    long nano_endTime = System.nanoTime();
+    return nano_endTime - nano_startTime;
+  }
 }

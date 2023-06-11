2a9050afefe70b87cfdf6d68e380c5f4cb5a465d
JamesCordova <jcordovas@unsa.edu.pe>
2023-06-11 10:29:31 -0500

    Nodo end agregado para tener un puntero al final y metodo para poner primero

--- Ejercicio2/DoublelinkList.java
+++ Ejercicio2/DoublelinkList.java
@@ -1,19 +1,25 @@
 public class DoubleLinkList<T>{
   Node<T> head;
+  Node<T> end;
   public DoubleLinkList(){
     head = null;
+    end = null;
+  }
+  public void addFirst(T data){
+    Node<T> aux = new Node<T>(data, head.getNextNode(), head);
+    head.setNextNode(aux);
+      
   }
   public void add(T data){
     Node<T> aux = new Node<T>(data);
     if(this.isEmpty()) 
       head = aux;
-    else{
-      head.se1tNextNode(aux);
-    }
+    else
+      end.setNextNode(aux);
       
   }
   public boolean isEmpty(){
-    return head == null;
+    return end == null;
   }
   
 }

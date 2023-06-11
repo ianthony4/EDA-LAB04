f82e61c2be6e9c54a8e3f182d1f675a2d2d02186
JamesCordova <jcordovas@unsa.edu.pe>
2023-06-11 11:58:56 -0500

    Correcion del valor de retorno del metodo

--- Ejercicio2/Test.java
+++ Ejercicio2/Test.java
@@ -1,19 +1,10 @@
 public class Test{
   public static void main(String[] args){
-    DoubleLinkList<Integer> nums = new DoubleLinkList<Integer>();
-    nums.addFirst(12);
-    nums.add(100);
-    nums.add(4);
-    nums.addFirst(9);
-    nums.addFirst(0);
-    System.out.println("Esta vacio? " + nums.isEmpty());
+    DoubleLinkList<Integer> nums = generarPeorCaso(10);
     System.out.println("Elementos " + nums);
-    nums.delFirst();
-    nums.delLast();
-    nums.delLast();
     System.out.println("Elementos " + nums);
   }
-  public static int[] generarPeorCaso(int t) {
+  public static DoubleLinkList<Integer> generarPeorCaso(int t) {
     DoubleLinkList<Integer> nums = new DoubleLinkList<Integer>();
     for (int i = 0; i < t; i++) {
       nums.add(t-i);

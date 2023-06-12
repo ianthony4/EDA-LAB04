c60b4146bda0dafacd1dadf182ed99efb0491409
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-06-11 20:33:35 -0500

    Override: isEmpty

--- Ejercicio1/LinkedList.java
+++ Ejercicio1/LinkedList.java
@@ -1,4 +1,12 @@
 public class LinkedList<E extends Comparable<E>> implements TDAList<E> {
     Node<E> first;
     int size;
+    
+    //Metodos de TDAList: proporcionando una la lógica real para cada uno de estos métodos.
+    
+    @Override
+    public boolean isEmpty(){
+        return this.first == null;
+    }
+    
 }

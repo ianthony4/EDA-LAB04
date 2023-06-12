7cd1f2e4e11f5182989222c4e62b65a558ac6b23
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-06-11 20:34:35 -0500

    Creando el constructor con tamaño 0 (size)

--- Ejercicio1/LinkedList.java
+++ Ejercicio1/LinkedList.java
@@ -2,11 +2,16 @@ public class LinkedList<E extends Comparable<E>> implements TDAList<E> {
     Node<E> first;
     int size;
     
+    //Constructor
+    public LinkedList() {
+        this.size = 0;
+    }
+    
     //Metodos de TDAList: proporcionando una la lógica real para cada uno de estos métodos.
     
     @Override
     public boolean isEmpty(){
         return this.first == null;
-    }
+    } 
     
 }

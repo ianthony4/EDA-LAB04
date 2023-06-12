5f2892e1c7e49a7ceb345d96d123ada7a1916b87
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-06-11 20:24:40 -0500

    Añadiendo comentarios a la clase, para un mejor entendimiento

--- Ejercicio1/TDAList.java
+++ Ejercicio1/TDAList.java
@@ -1,14 +1,23 @@
+//Esta es una interface que especifica las operaciones basicas que se 
+//pueden realizar en una lista generica. 
+
 public interface TDAList<E> {
+    //verifica si la lista esta vacia
     boolean isEmpty();
 
+    //inserta un elemento x al inicio de la lista
     void insertFirst(E x);
 
+    //inserta un elemento x al final de la lista
     void insertLast(E x);
 
+    //Busca el elemento X, true si la encuentra, false si no encuentra
     boolean search(E x);
 
+    //Elimina el elemento x, si lo encuentra lo hace, caso contrario no pasa nada
     void remove(E x);
 
+    //Este metodo inserta el elemento X en la posicion P de la lista
     void insert(E x, int p);
 
 }
